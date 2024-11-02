acc760e5fa1e135a77d4d0a934839b9f36e3ff7f
Tenzin <chaksamt@gmail.com>
2024-11-02 03:14:41 -0500

    init

---
+++ projects/footer/src/lib/footer.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-footer',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      footer works!
+    </p>
+  `,
+  styles: ``
+})
+export class FooterComponent {
+
+}
---
+++ projects/footer/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of footer
+ */
+
+export * from './lib/footer.service';
+export * from './lib/footer.component';
